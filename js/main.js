b3d38ba2bc3cb16b9268a247ae9ad57bd258d1e7
sowhyy <sowhyy90@gmail.com>
2023-08-31 12:31:54 +0900

    v1.2.0
    캡쳐용 화면 추가

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-$('#popup').modal('show')
-
 (function () {
   "use strict";
 
@@ -99,4 +97,41 @@ $('#popup').modal('show')
 
 
 });
+
+
+
+const Grid = tui.Grid;
+const instance = new Grid({
+  el: document.getElementById("grid"), // Container element
+  columns: [
+    {
+      header: "Name",
+      name: "name",
+    },
+    {
+      header: "Artist",
+      name: "artist",
+    },
+    {
+      header: "Release",
+      name: "release",
+    },
+    {
+      header: "Genre",
+      name: "genre",
+    },
+  ],
+  data: [
+    {
+      name: "Beautiful Lies",
+      artist: "Birdy",
+      release: "2016.03.26",
+      genre: "Pop",
+    },
+  ],
+});
+
+instance.resetData(newData); // Call API of instance's public method
+Grid.applyTheme("striped"); // Call API of static method
+
 
